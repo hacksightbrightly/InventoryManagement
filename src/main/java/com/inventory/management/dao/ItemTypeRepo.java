65496a6267815531abea099c6d8f4b1d775780ef
Ishan Srivastava <103515598+hacksightbrightly@users.noreply.github.com>
2022-06-02 18:39:56 +0530

    Update ItemTypeRepo.java

--- src/main/java/com/inventory/management/dao/ItemTypeRepo.java
+++ src/main/java/com/inventory/management/dao/ItemTypeRepo.java
@@ -2,7 +2,6 @@ package com.inventory.management.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.inventory.management.controller.ItemType;
 import com.inventory.management.entities.Classification;
 import com.inventory.management.entities.ItemType;
 public interface ItemTypeRepo extends CrudRepository<ItemType,Integer>{
